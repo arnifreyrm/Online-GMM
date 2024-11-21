c39c3e89d90968b2871222aab7130b895ef220e4
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Pro-2.local>
2024-11-21 12:54:20 -0500

    moved util.py to functions, added skeleton for loss

---
+++ functions/loss.py
@@ -0,0 +1,14 @@
+
+
+def get_log_likelihood_individual(param1, param2):
+    # calculate LL Loss between two individual params
+    return
+
+def get_loss_matrix():
+    # compute the loss matrix between cluster i in param set 1 and param set 2
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.linear_sum_assignment.html
+    return
+
+def log_likliehood():
+    # find the most likely alignment between parameter sets by calculating cost matrix
+    return
